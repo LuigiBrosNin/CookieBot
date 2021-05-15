8eb50f6e174c022e7622c367618dfc38b9a65e7b
LuigiBrosNin <76620186+LuigiBrosNin@users.noreply.github.com>
2021-05-15 14:57:40 +0200

    game uppdate
    and modcookie fix

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ function writeUsers(){ //Copies users and cookie jars into the file
 }
 
 function modcookie(username, amount) { //adds an amount of cookies in the username's cookiejar
-    if (users[username] < 0) users[username] = 0
     users[username] += amount
+    if (users[username] < 0) users[username] = 0
     writeUsers()
 }
 
@@ -220,9 +220,171 @@ bot.onText(/\/coockiechance/, (msg) =>{
             bot.sendMessage(chatid,"life gave you lemons, so you sold them and bought more cookies.\n@"+nicc+" gains 8🍪 and a lemonade stand")
             modcookie(nicc, 8)
             break;
-        case 9: //continue from here
-            bot.sendMessage(chatid,"\n@"+nicc+" gains 9🍪 and a succubus that can bake cookies")
+        case 9:
+            bot.sendMessage(chatid,"you found a cup of water from the long lost fountain of youth. you use that water as ingredient for the cookies you were backing\n@"+nicc+" gains 9🍪 and feels a lil older")
+            modcookie(nicc, 9)
+            break;
+        case 10:
+            bot.sendMessage(chatid,"you posted a meme everyone enjoyed.\n@"+nicc+" gains 3🍪 and the developer of this bot's approval")
+            modcookie(nicc, 3)
+            break;
+        case 11:
+            bot.sendMessage(chatid,"you hacked into this bot and generated yourself some cookies.\n@"+nicc+" gains 9🍪 and you'd better hope admins don't catch you")
+            modcookie(nicc, 9)
+            break;
+        case 12:
+            bot.sendMessage(chatid,"you make your way through Hogwarts just to learn how to spawn cookies.\n@"+nicc+" gains 4🍪 and a magic wand")
+            modcookie(nicc, 4)
+            break;
+        case 13:
+            bot.sendMessage(chatid,"You offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nYou offer to the shrine, but gain nothing.\nin the end you say \"fuck it\" and open a random chest next to the shrine. it had some cookies.\n@"+nicc+" gains 2🍪 and a gambling addiction")
+            modcookie(nicc, 2)
+            break;
+        case 14:
+            bot.sendMessage(chatid,"your life is so miserable, they assign you fairy godparents. you wish the only thing worth wishing.\n@"+nicc+" gains 1🍪, but you don't keep the fairies.\ni'm sorry.")
+            modcookie(nicc, 1)
+            break;
+        case 15:
+            bot.sendMessage(chatid,"this message has only 1% chance to pop up and you were so lucky.\n@"+nicc+" gains 1🍪 and now knows a secret.\n that secret is how many prompts the developer wrote for this stupid game")
+            modcookie(nicc, 1)
+            break;
+        case 16:
+            bot.sendMessage(chatid,"you unistalled league of legends.\n@"+nicc+" gains 3🍪 and your soul back")
+            modcookie(nicc, 3)
+            break;
+        case 17:
+            bot.sendMessage(chatid,"you finally pass the logic exam.\n@"+nicc+" gains 999🍪 and 9 CFU.\n(just kidding, you only gain 5. i'm sorry)")
+            modcookie(nicc, 5)
+            break;
+        case 18:
+            bot.sendMessage(chatid,"you start a notion, gaining everyone's trust and respect.\n@"+nicc+" gains 8🍪")
+            modcookie(nicc, 8)
+            break;
+        case 19:
+            bot.sendMessage(chatid,"you discover that cookie clicker exists.\n@"+nicc+" gains 1🍪 and addiction to the game")
+            modcookie(nicc, 1)
+            break;
+        case 20:
+            bot.sendMessage(chatid,"you start working in a cookie mine as cookiedigger, it pays off.\n@"+nicc+" gains 4🍪 and Klondike nostalgia")
+            modcookie(nicc, 4)
+            break;
+        case 21:
+            bot.sendMessage(chatid,"you open up a factory to produce cookies. it bankrupts the next day, but  you keep all production.\n@"+nicc+" gains 6🍪")
             modcookie(nicc, 6)
+            break;
+        case 22:
+            bot.sendMessage(chatid,"you invest some cookies in the cookie stock market. it pays off.\n@"+nicc+" gains 3🍪 and rising stonks")
+            modcookie(nicc, 3)
+            break;
+        case 23:
+            bot.sendMessage(chatid,"you build a temple to honor the cookie god. there's no such thing as a cookie god, but you keep the offerings.\n@"+nicc+" gains 4🍪 and a mitre hat with a cookie on it.")
+            modcookie(nicc, 4)
+            break;
+        case 24:
+            bot.sendMessage(chatid,"you become CEO of tesla and nasa to explore the universe searching for the legendary cookie planet. banks give full funding on the prject.\n@"+nicc+" gains 7🍪 (and a meme lord)")
+            modcookie(nicc, 7)
+            break;
+        case 25:
+            bot.sendMessage(chatid,"you find the philosopher's cookie, that turns gold into cookies. yay!\n@"+nicc+" gains 6🍪, but your mom can't find her wedding ring")
+            modcookie(nicc, 6)
+            break;
+        case 26:
+            bot.sendMessage(chatid,"you manage to open up a portal to the cookieverse. you manage to grab some cookies before the portal exploded.\n@"+nicc+" gains 2🍪 and an invasion of cookie monsters")
+            modcookie(nicc, 2)
+            break;
+        case 27:
+            bot.sendMessage(chatid,"you discover time traver to bring here cookies before they were eaten.\n@"+nicc+" gains 5🍪 and a time paradox")
+            modcookie(nicc, 5)
+            break;
+        case 28:
+            bot.sendMessage(chatid,"you gain control of animatter, so you convert it into cookies.\n@"+nicc+" gains 4🍪")
+            modcookie(nicc, 4)
+            break;
+        case 29:
+            bot.sendMessage(chatid,"you manage to convert light itself into cookies. the universe is now pitch black, but you've gained some cookies. it's just hard to find them in the dark\n@"+nicc+" gains 9🍪")
+            modcookie(nicc, 9)
+            break;
+        case 30:
+            bot.sendMessage(chatid,"you learn clonation is a thing, so you spend billions into cloning some cookies.\n@"+nicc+" gains 2🍪 and a crippling debt")
+            modcookie(nicc, 2)
+            break;
+        case 31:
+            bot.sendMessage(chatid,"you find a cookie on the ground. you better sanitize it before putting it in your cookiejar.\n@"+nicc+" gains 1🍪 and some soap")
+            modcookie(nicc, 1)
+            break;
+        case 32:
+            bot.sendMessage(chatid,"you buy a fortune cookie, and inside it instead of an advice there's another cookie!\n@"+nicc+" gains 2🍪 and possible future bad choices")
+            modcookie(nicc, 2)
+            break;
+        case 33:
+            bot.sendMessage(chatid,"a stranger offers you a cookie. you accept despite mom's warnings.\n@"+nicc+" gains 1🍪 and a drug addiction")
+            modcookie(nicc, 1)
+            break;
+        case 34:
+            bot.sendMessage(chatid,"you ask santa Claus for cookies. you learn he isn't real, but you can keep the cookies you left for him.\n@"+nicc+" gains 3🍪 and a cup of milk")
+            modcookie(nicc, 3)
+            break;
+        case 35:// gap i left cuz i'm waiting for some other possibly good commands from alice
+            bot.sendMessage(chatid,"you learn how to actually bake cookies.\n@"+nicc+" gains 5🍪 and a new skill")
+            modcookie(nicc, 5)
+            break;
+        case 40:
+            bot.sendMessage(chatid,"while you were taking your cookiejar out for a walk, you trip, letting some cookies fly away.\n@"+nicc+" loses 2🍪")
+            modcookie(nicc, -2)
+            break;
+        case 41:
+            bot.sendMessage(chatid,"a zombie invades your house, eating your cookies. another one eats your cousin's brain, but who cares.\n@"+nicc+" loses 3🍪 and a cousin")
+            modcookie(nicc, -3)
+            break;
+        case 42:
+            bot.sendMessage(chatid,"you ask santa Claus for cookies.\nhe saw your internet history.\n@"+nicc+" loses 1🍪 but you learn to use incognito")
+            modcookie(nicc, -1)
+            break;
+        case 43:
+            bot.sendMessage(chatid,"you die. there's nothing after death, just endless darkness. someone steals some cookies from you. you come back to life knowing you cookies are in danger, but it's too late.\nwelp, at least you're alive.\n@"+nicc+" loses 2🍪 but gains depression")
+            modcookie(nicc, -2)
+            break;
+        case 44:
+            bot.sendMessage(chatid,"the succubus you summoned demands payment.\n@"+nicc+" loses 2🍪 and your soul is now tied to hell")
+            modcookie(nicc, -2)
+            break;
+        case 45:
+            bot.sendMessage(chatid,"since he worked so hard for this bot, you decide to give the developer a cookie to show your appreciation.\n@"+nicc+" loses 1🍪 but gains a personal thanks from the dev")
+            giveCookies(chatid,nicc,"LuigiBrosNin",1)
+            break;
+        case 46:
+            bot.sendMessage(chatid,"you're a simp. you donate some of your cookies to a twitch thot.\n@"+nicc+" loses 5🍪 and your dignity.")
+            modcookie(nicc, -5)
+            break;
+        case 47:
+            bot.sendMessage(chatid,"the lizard man asks to accept his cookies. you do, but he instantly sells all your info along with some of your cookies.\n@"+nicc+" loses 4🍪 and your privacy, but now you have a facebook account")
+            modcookie(nicc, -4)
+            break;
+        case 48:
+            var index = Math.floor(Math.random() * users.length)
+            bot.sendMessage(chatid,"@"+Object.keys(users)[index]+" found a way to hijack your cookiejar and stole a cookie!\n@"+nicc+" loses 1🍪\nbut @"+Object.keys(users)[index]+" gains a 🍪!")
+            giveCookies(chatid,nicc,Object.keys(users)[index], 1)
+            break;
+        case 49:
+            var index = Math.floor(Math.random() * users.length)
+            bot.sendMessage(chatid,"@"+Object.keys(users)[index]+" stole a cookie while you were in horny jail\n@"+nicc+" loses 1🍪\nbut @"+Object.keys(users)[index]+" gains a 🍪!")
+            giveCookies(chatid,nicc,Object.keys(users)[index], 1)
+            break;
+        case 50:
+            bot.sendMessage(chatid,"bro, that way pretty cringe ngl.\n@"+nicc+" loses 3🍪 and the \"memelord\" tag")
+            modcookie(nicc, -3)
+            break;
+        case 51:
+            bot.sendMessage(chatid,"your illegal cookie traffic got caught and the merch got confiscated.\n@"+nicc+" loses 4🍪 and you'll likely go to jail")
+            modcookie(nicc, -4)
+            break;
+        case 52:
+            bot.sendMessage(chatid,"literally nothing happens.\n@"+nicc+" loses 1🍪 cuz i'm evil")
+            modcookie(nicc, -1)
+            break;
+        case 53: //keep going from here.
+            bot.sendMessage(chatid,"\n@"+nicc+" loses 1🍪 cuz i'm evil")
+            modcookie(nicc, -1)
             break;
         default:
             break;
